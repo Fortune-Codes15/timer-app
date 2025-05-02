6f2a49b7309e20bd5c1a29b2a1e805ea98d826c9
Fortune <fortunefoluso26@gmail.com>
2025-05-02 15:12:12 +0100

    Added dark mode

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,1 +1,29 @@
-console.log( 'TIme to add a preference feature 😎' )
+const changePreference = document.querySelector( '.preference_text' );
+const body = document.querySelector( 'body' );
+const icons = document.querySelectorAll( 'button' );
+const inputs = document.querySelectorAll( 'input' )
+
+
+if ( localStorage.getItem( 'darkMode' ) === 'enabled' ) {
+  body.classList.add( 'dark' );
+  icons.forEach( icon => icon.classList.add( 'icons-dark' ) );
+  inputs.forEach( input => input.classList.add( 'input-dark' ) );
+}
+
+changePreference.addEventListener( 'click', () => {
+  body.classList.toggle( 'dark' );
+  icons.forEach( icon => icon.classList.toggle( 'icons-dark' ) );
+  inputs.forEach( input => input.classList.toggle( 'input-dark' ) );
+
+  const isDarkMode = body.classList.contains( 'dark' );
+  localStorage.setItem( 'darkMode', isDarkMode ? 'enabled' : 'disabled' );
+} );
+
+
+const user = 'Fortune';
+
+s
+localStorage.setItem( 'user', JSON.stringify( user ) );
+
+const storedUSer = JSON.parse( localStorage.getItem( 'user' ) )
+console.log( storedUSer )
